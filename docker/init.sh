7a0b6f1cc92bd60946c15acf86db6716292ef852
Kubernetes Coder <coder80@openshift.red>
2017-08-04 13:33:19 +0100

    added dynamic creation of block volumes if DEVS variable passed

--- docker/init.sh
+++ docker/init.sh
@@ -5,16 +5,33 @@ mkdir /run/dbus
 dbus-daemon --system
 
 # iscsi vars
-TARGET_NAME='iqn.2016-04.test.com:storage.target00'
-ACL_IQN='iqn.2016-04.test.com:test.img'
-AUTH_USER_ID=5f84cec2
-AUTH_PASSWORD=b0d324e9
+[ -z ${TARGET_NAME} ] && TARGET_NAME='iqn.2016-04.test.home:storage.target00'
+[ -z ${ACL_IQN} ] && ACL_IQN='iqn.2016-04.test.com:test'
 
-mkdir /iscsi_disks
+DISKID=1
+if [ -z ${DEVS} ] ; then
 
-# Create backstores
-targetcli /backstores/fileio create disk01 /iscsi_disks/disk01.img 2G
-targetcli /backstores/fileio create disk02 /iscsi_disks/disk02.img 2G
+  mkdir /iscsi_disks
+  DEV="/iscsi_disks/disk01.img /iscsi_disks/disk02.img"
+
+  for dev in ${DEVS}; do
+
+    targetcli /backstores/fileio create disk${DISKID} ${dev} 2G
+    DISKID=$((DISKID+1))
+
+  done
+
+else
+
+  for dev in ${DEVS}; do
+
+    targetcli /backstores/block create disk${DISKID} ${dev}
+    DISKID=$((DISKID+1))
+
+  done
+
+fi
+
 # Create iscsi target
 targetcli /iscsi create ${TARGET_NAME}
 # Set IP address of the target
@@ -25,12 +42,17 @@ targetcli /iscsi/${TARGET_NAME}/tpg1/luns create /backstores/fileio/disk01
 targetcli /iscsi/${TARGET_NAME}/tpg1/luns create /backstores/fileio/disk02
 # Set ACL
 targetcli /iscsi/${TARGET_NAME}/tpg1/acls create ${ACL_IQN}
+
 # Set auth
-targetcli /iscsi/${TARGET_NAME}/tpg1/acls/${ACL_IQN} set auth userid=${AUTH_USER_ID}
-targetcli /iscsi/${TARGET_NAME}/tpg1/acls/${ACL_IQN} set auth password=${AUTH_PASSWORD}
+#AUTH_USER_ID=5f84cec2
+#AUTH_PASSWORD=b0d324e9
+#targetcli /iscsi/${TARGET_NAME}/tpg1/acls/${ACL_IQN} set auth userid=${AUTH_USER_ID}
+#targetcli /iscsi/${TARGET_NAME}/tpg1/acls/${ACL_IQN} set auth password=${AUTH_PASSWORD}
 
 
 while true
 do
+    date
+    targetcli sessions list
     sleep 30
 done
